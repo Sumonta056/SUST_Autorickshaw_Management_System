45c11402652b45ca1b38133fb1dd365732f9cd36
Sumonta Saha Mridul <61287791+Sumonta056@users.noreply.github.com>
2023-09-25 21:03:51 +0600

    [update] Code Base

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,9 @@ import Registration from "./components/Registration/Registration";
 import OwnerRegistration from "./components/OwnerRegistration/OwnerRegistration";
 import DriverRegistration from "./components/DriverRegistration/DriverRegistration";
 import Home from "./components/Home/Home";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import Student from "./components/CRUD/practice";
+import Create from "./components/CRUD/CreateStudent";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -15,6 +18,11 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Registration />} />
         <Route path="/OwnerRegistration" element={<OwnerRegistration />} />
+
+        <Route path="/student" element={<Student />} />
+        <Route path="/create" element={<Create />} />
+
+        <Route path="/DriverRegistration" element={<DriverRegistration />} />
       </Routes>
     </BrowserRouter>
   );
