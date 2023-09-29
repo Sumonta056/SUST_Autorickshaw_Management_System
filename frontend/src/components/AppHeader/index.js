3657ff7a91abc55c5a7c4a0ad6e5fd57b130ba05
Sumonta Saha Mridul <61287791+Sumonta056@users.noreply.github.com>
2023-09-29 21:13:24 +0600

    [fix] App Header Frontend for dashboard

--- frontend/src/components/AppHeader/index.js
+++ frontend/src/components/AppHeader/index.js
@@ -5,9 +5,9 @@ function AppHeader() {
   return (
     <div className="AppHeader">
       <div className="AppHeaderTitle">
-        <Image width={40} className="AppHeaderImg" src="./logo.png"></Image>
+        <Image width={35} className="AppHeaderImg" src="./logo.png"></Image>
         <div className="AppHeaderTitle2">
-          SUST Auto-Rickshaw Management System
+        শাবিপ্রবি অটোরিকশা ম্যানেজমেন্ট সিস্টেম
         </div>
       </div>
     </div>
